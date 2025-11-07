66c5e4c3ce3f999f3841e42160bb98e521b917df
Erdem Çelik <erdemcelik212@icloud.com>
2025-11-07 13:14:06 +0300

    Inıtial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace modern_calculator_app.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace modern_calculator_app.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
