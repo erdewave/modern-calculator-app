27dd80671df9fec3d8210e0f58e541f4067f368d
Erdem Çelik <erdemcelik212@icloud.com>
2025-11-07 18:55:43 +0300

    feat(ui): Improve UI layout

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using modern_calculator_app.ViewModels;
 
 namespace modern_calculator_app.Views;
 
@@ -7,5 +9,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel();
     }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
 }
